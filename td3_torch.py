1e9281c5ee12a1deaadf112944be69469107a358
isil <isilozgu2016@gmail.com>
2025-01-15 18:18:39 +0300

    add instructions

--- td3_torch.py
+++ td3_torch.py
@@ -1,3 +1,21 @@
+"""
+Isil Ozgu 22102276
+PA3 - TD3 on LunarLanderContinuous-v2
+
+This code is the implementation of TD3 algorithm on paper:https://arxiv.org/abs/1802.09477
+I have inspected their code on https://github.com/sfujim/TD3/ and utilize their implementation techniques and
+old hyperparameters.
+
+Since this was my first attempt to implementing neural networks and PG, extra resources were used:
+Pytorch Neural Network playlist: https://youtube.com/playlist?list=PLCC34OHNcOtpcgR9LEYSdi9r7XIbpkpK1&si=v8UbDfb15aGlDF-f
+TD3 tutorial: https://youtu.be/ZhFO8EWADmY?si=RGNLH9i5O2Tkq3Sh&t=298
+
+To run the code successfully, create /plots and /tmp/td3 dictionaries in the root directory where this file exists
+if they don't exist already. These are needed to save the plots and weights. Then run this file using your favorite IDE.
+Since I was using Windows as my OS the code does not include the rendering of the results.
+
+"""
+
 import numpy as np
 import torch as T
 import torch.nn as nn
@@ -9,23 +27,108 @@ import gym
 import matplotlib.pyplot as plt
 
 
-class ReplayBuffer:
+class Actor(nn.Module):
+    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, action_dims):
+        super(Actor, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.action_dims = action_dims
+
+        self.name = None
+        self.checkpoint_file = None
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.pi = nn.Linear(self.fc2_dims, self.action_dims)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def set_name(self, name, chkpt_dir='tmp/td3'):
+        # needed after deep copying the components
+        self.name = name
+        self.checkpoint_file = os.path.join(chkpt_dir, name + '_td3')
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = T.tanh(self.pi(x))  # bounds -1 +1
+        return x
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        if os.path.isfile(self.checkpoint_file):
+            self.load_state_dict(T.load(self.checkpoint_file))
+        else:
+            print(f"Checkpoint file {self.checkpoint_file} not found. Initializing with random weights.")
+
+
+class Critic(nn.Module):
+    def __init__(self, beta, state_dims, fc1_dims, fc2_dims, action_dims):
+        super(Critic, self).__init__()
+        self.name = None
+        self.checkpoint_file = None
+
+        self.state_dims = state_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.action_dims = action_dims
+
+        self.fc1 = nn.Linear(self.state_dims[0] + action_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q1 = nn.Linear(self.fc2_dims, 1)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=beta)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def set_name(self, name, chkpt_dir='tmp/td3'):
+        # needed after deep copying the components
+        self.name = name
+        self.checkpoint_file = os.path.join(chkpt_dir, name + '_td3')
+
+    def forward(self, state, action):
+        x = self.fc1(T.cat([state, action], dim=1))
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+        x = self.q1(x)
+        return x
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        if os.path.isfile(self.checkpoint_file):
+            self.load_state_dict(T.load(self.checkpoint_file))
+        else:
+            print(f"Checkpoint file {self.checkpoint_file} not found. Initializing with random weights.")
+
+
+class Buffer:
     # input shape is a tuple and unpack it and send it to np.zeros
-    # we are on continuous dimension, so n_action is actually is the "number of components of an action"
-    def __init__(self, max_size, input_dims, n_actions):
-        self.mem_size = max_size
+    def __init__(self, max_size, state_dims, action_dims):
+        self.max_size = max_size
         self.mem_counter = 0  # where to put new mem
         self.device = T.device("cuda" if T.cuda.is_available() else "cpu")
         self.current_size = 0
 
-        self.state_memory = np.zeros((self.mem_size, *input_dims))
-        self.next_state_memory = np.zeros((self.mem_size, *input_dims))
-        self.reward_memory = np.zeros(self.mem_size)
-        self.action_memory = np.zeros((self.mem_size, n_actions))
-        self.terminal_memory = np.zeros(self.mem_size, dtype=bool)  # done flags of environment
+        self.state_memory = np.zeros((self.max_size, *state_dims))
+        self.next_state_memory = np.zeros((self.max_size, *state_dims))
+        self.reward_memory = np.zeros(self.max_size)
+        self.action_memory = np.zeros((self.max_size, action_dims))
+        self.terminal_memory = np.zeros(self.max_size, dtype=bool)  # done flags of environment
 
     def store_transition(self, state, action, reward, next_state, done):
-        index = self.mem_counter % self.mem_size  # circular buffer
+        index = self.mem_counter % self.max_size  # circular buffer
 
         self.state_memory[index] = state
         self.next_state_memory[index] = next_state
@@ -35,7 +138,7 @@ class ReplayBuffer:
 
         index += 1
         self.mem_counter = index
-        self.current_size = min(self.mem_size, self.current_size + 1)
+        self.current_size = min(self.max_size, self.current_size + 1)
 
     def sample(self, batch_size):
         batch = np.random.choice(self.current_size, batch_size)  # ensure not to sample zeros
@@ -55,107 +158,16 @@ class ReplayBuffer:
         )
 
 
-class CriticNetwork(nn.Module):
-    def __init__(self, beta, input_dims, fc1_dims, fc2_dims, n_actions):
-        super(CriticNetwork, self).__init__()
-        self.name = None
-        self.checkpoint_file = None
-
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
-
-        self.fc1 = nn.Linear(self.input_dims[0] + n_actions, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.q1 = nn.Linear(self.fc2_dims, 1)
-
-        self.optimizer = optim.Adam(self.parameters(), lr=beta)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-
-        self.to(self.device)
-
-    def set_name(self, name, chkpt_dir='tmp/td3'):
-        # needed after deep copying the components
-        self.name = name
-        self.checkpoint_file = os.path.join(chkpt_dir, name + '_td3')
-
-    def forward(self, state, action):
-        q1_action_value = self.fc1(T.cat([state, action], dim=1))
-        q1_action_value = F.relu(q1_action_value)
-        q1_action_value = self.fc2(q1_action_value)
-        q1_action_value = F.relu(q1_action_value)
-
-        q1 = self.q1(q1_action_value)
-
-        return q1
-
-    def save_checkpoint(self):
-        print('save checkpoint')
-        T.save(self.state_dict(), self.checkpoint_file)
-
-    def load_checkpoint(self):
-        if os.path.isfile(self.checkpoint_file):
-            print(f"Loading checkpoint from {self.checkpoint_file}")
-            self.load_state_dict(T.load(self.checkpoint_file))
-        else:
-            print(f"Checkpoint file {self.checkpoint_file} not found. Initializing with random weights.")
-
-
-class ActorNetwork(nn.Module):
-    def __init__(self, alpha, input_dims, fc1_dims, fc2_dims, n_actions):
-        super(ActorNetwork, self).__init__()
-        self.input_dims = input_dims
-        self.fc1_dims = fc1_dims
-        self.fc2_dims = fc2_dims
-        self.n_actions = n_actions
-        self.name = None
-        self.checkpoint_file = None
-
-        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
-        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
-        self.pi = nn.Linear(self.fc2_dims, self.n_actions)
-
-        self.optimizer = optim.Adam(self.parameters(), lr=alpha)
-        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-
-        self.to(self.device)
-
-    def set_name(self, name, chkpt_dir='tmp/td3'):
-        # needed after deep copying the components
-        self.name = name
-        self.checkpoint_file = os.path.join(chkpt_dir, name + '_td3')
-
-    def forward(self, state):
-        x = self.fc1(state)
-        x = F.relu(x)
-        x = self.fc2(x)
-        x = F.relu(x)  # buna gerek yok mu?
-
-        pi = T.tanh(self.pi(x))  # bounds -1 +1
-
-        return pi
-
-    def save_checkpoint(self):
-        print('save checkpoint')
-        T.save(self.state_dict(), self.checkpoint_file)
-
-    def load_checkpoint(self):
-        if os.path.isfile(self.checkpoint_file):
-            print(f"Loading checkpoint from {self.checkpoint_file}")
-            self.load_state_dict(T.load(self.checkpoint_file))
-        else:
-            print(f"Checkpoint file {self.checkpoint_file} not found. Initializing with random weights.")
-
-
 class TD3:
-    def __init__(self, alpha, beta, input_dims, n_actions, capital_t, batch_size, discount, run_env, d, tau, noise_std,
+    def __init__(self, alpha, beta, state_dimensions, action_dimensions, capital_t, batch_size, discount, run_env, d,
+                 tau, noise_std,
                  initial_pure_exploration_limit):
-        self.buffer = ReplayBuffer(int(1e6), input_dims, n_actions)
 
-        self.actor = ActorNetwork(alpha, input_dims, 400, 300, n_actions)
-        self.critic1 = CriticNetwork(beta, input_dims, 400, 300, n_actions)
-        self.critic2 = CriticNetwork(beta, input_dims, 400, 300, n_actions)
+        self.buffer = Buffer(int(1e6), state_dimensions, action_dimensions)
+
+        self.actor = Actor(alpha, state_dimensions, 400, 300, action_dimensions)
+        self.critic1 = Critic(beta, state_dimensions, 400, 300, action_dimensions)
+        self.critic2 = Critic(beta, state_dimensions, 400, 300, action_dimensions)
 
         self.target_actor = copy.deepcopy(self.actor)
         self.target_actor.set_name("target_actor")
@@ -169,7 +181,7 @@ class TD3:
         self.critic2.set_name("critic2")
 
         self.score_hist = []
-        self.iteration_count = -1
+        self.iteration_count = 0
         self.capital_t = capital_t
         self.batch_size = batch_size
         self.discount = discount
@@ -190,14 +202,14 @@ class TD3:
             state = state[0]
 
         total_score = 0
-        avg_score = 0
+        avg_score_ = 0
 
         for t in range(self.capital_t):
 
             # select action w noise
             action = None
 
-            if t < self.initial_pure_exploration_limit:
+            if self.iteration_count < self.initial_pure_exploration_limit:
                 action = self.env.action_space.sample()
             else:
                 state = T.FloatTensor(state).to(self.actor.device)
@@ -209,46 +221,45 @@ class TD3:
             # observe reward and next state
             next_state_observed, reward_observed, terminal_observed, truncated_observed, info = self.env.step(action)
 
+            # update the total score of episode
             total_score += reward_observed
             self.score_hist.append(total_score)
-            avg_score = np.mean(self.score_hist[-100:])
-
-
+            avg_score_ = np.mean(self.score_hist[-100:])
 
             print('episode ', t, 'score %.1f' % total_score,
-                  'average score %.1f' % avg_score)
+                  'average score %.1f' % avg_score_)
 
-            # store transition tuple
+            # update the state if it's not in the assumed shape
             if isinstance(state, tuple):
                 state = state[0]
 
+            # store transition tuple
             self.buffer.store_transition(state, action, reward_observed, next_state_observed, terminal_observed)
 
-            # sample mini batch
             # train after collecting sufficient data
             if t < self.batch_size:
+                state = next_state_observed
+                # terminate episode if terminal state is next
                 if terminal_observed or truncated_observed:
-                    self.iteration_count = t
-                    break
+                    self.iteration_count += 1
+                    return avg_score_
                 continue
 
+            # sample mini batch
             states, actions, next_states, rewards, terminals = self.buffer.sample(self.batch_size)
 
             # noisy action from target actor
             noisy_target_actions = self.target_actor.forward(next_states) + \
                                    T.clamp(T.tensor(np.random.normal(scale=0.2)), -0.5, 0.5)
-            scaled_noisy_target_actions = T.clamp(noisy_target_actions, self.min_action[0],
-                                                  self.max_action[
-                                                      0])  # ensure noisy actions are in the bounds of action space
+            scaled_noisy_target_actions = T.clamp(noisy_target_actions, self.min_action[0], self.max_action[0])
+            # ensure noisy actions are in the bounds of action space
 
             # calculate target value
             q1_target = self.target_critic1.forward(next_states, scaled_noisy_target_actions)
             q2_target = self.target_critic2.forward(next_states, scaled_noisy_target_actions)
 
-            # q1 = q1.view(-1)
-            # q2 = q2.view(-1)
-
-            target = rewards + self.discount * (T.min(q1_target, q2_target)) * terminals
+            target = rewards + self.discount * (T.min(T.flatten(q1_target), T.flatten(q2_target))) * (
+                        1 - terminals.float())
 
             # update critics
             q1 = self.critic1.forward(states, actions)
@@ -259,6 +270,7 @@ class TD3:
             self.critic2.optimizer.zero_grad()
 
             # optimize
+            target = target.reshape(self.batch_size, 1)  # there was a dimensionality mismatch
             critic_loss = F.mse_loss(target, q1) + F.mse_loss(target, q2)
             critic_loss.backward()
             self.critic1.optimizer.step()
@@ -283,15 +295,16 @@ class TD3:
                     target_param.data.copy_(self.tau * param.data + (1 - self.tau) * target_param.data)
 
             state = next_state_observed
+            self.iteration_count += 1
             if terminal_observed or truncated_observed:
-                self.iteration_count = t
-                if avg_score > best_score_given:
+
+                if avg_score_ > best_score_given:
                     agent.save_models()
-                return avg_score
+                return avg_score_
         else:
-            if avg_score > best_score_given:
+            if avg_score_ > best_score_given:
                 agent.save_models()
-            return avg_score
+            return avg_score_
 
     def save_models(self):
         self.actor.save_checkpoint()
@@ -314,7 +327,7 @@ if __name__ == '__main__':
     total_game_count = 1000
     x = [i + 1 for i in range(total_game_count)]
     seeds = [42, 1337, 256, 9876, 2021, 999]
-    filename = 'plots/LunarLanderContinuous_{}_games.png'.format(total_game_count)
+    filename = 'plots/TD3_LunarLander_Continuous_{}_games.png'.format(total_game_count)
     all_scores = []
 
     for seed in seeds:
@@ -325,13 +338,15 @@ if __name__ == '__main__':
         np.random.seed(seed)
 
         agent = TD3(alpha=0.001, beta=0.001,
-                    input_dims=env.observation_space.shape, tau=0.005,
+                    state_dimensions=env.observation_space.shape, tau=0.005,
                     run_env=env, batch_size=100, discount=0.99, d=2, noise_std=0.1,
-                    n_actions=env.action_space.shape[0], capital_t=10000, initial_pure_exploration_limit=1000)
+                    action_dimensions=env.action_space.shape[0], capital_t=10000, initial_pure_exploration_limit=300)
 
         agent.load_models()
         best_score = env.reward_range[0]
         scores = []
+
+        # run the episodes
         for i in range(total_game_count):
             avg_score = agent.run(best_score)
             if avg_score > best_score:
@@ -346,7 +361,7 @@ if __name__ == '__main__':
         # save the plot for the current seed
         plt.plot(x, running_avg)
         plt.title(f'Running average of previous 100 scores (Seed {seed})')
-        plt.savefig(f'plots/LunarLanderContinuous_with_seed{seed}.png')
+        plt.savefig(f'plots/LunarLanderContinuous_with_seed{seed}_game{total_game_count}.png')
         plt.clf()
 
         all_scores.append(scores)
